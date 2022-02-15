cd09b6a9c7a3647c0b74d6a0418b20d6955f4075
Murat <mrdudov@gmail.com>
2022-02-15 18:57:09 +0300

    image file preview

--- upload.js
+++ upload.js
@@ -1,1 +1,58 @@
-console.log('upload.js')
+import { DefaultDeserializer } from "v8"
+
+export function upload(selector, options = {}) {
+    const input = document.querySelector(selector)
+    const preview = document.createElement('div')
+
+    preview.classList.add('preview')
+
+    const open = document.createElement('button')
+    open.classList.add('btn')
+    open.textContent = 'Открыть'
+
+    if (options.multi) {
+        input.setAttribute('multiple', true)
+    }
+
+    if (options.accept && Array.isArray(options.accept)) {
+        input.setAttribute('accept', options.accept.join(','))
+    }
+
+    input.insertAdjacentElement('afterend', preview)
+    input.insertAdjacentElement('afterend', open)
+
+    const triggerInput = () => input.click()
+
+    const changeHandler = event => {
+        if (!event.target.files.length) {
+            return
+        }
+
+        const files = Array.from(event.target.files)
+        
+        preview.innerHTML = ''
+
+        files.forEach(file => {
+            if (!file.type.match('image')) {
+                return
+            }
+
+            const reader = new FileReader()
+
+            reader.onload = ev => {
+                const src = ev.target.result
+                preview.insertAdjacentHTML('afterbegin', `
+                <div class="preview-image">
+                    <img src="${src}" alt="${file.name}" />
+                </div>
+                `)
+            }
+
+            reader.readAsDataURL(file)
+        })
+    }
+
+
+    open.addEventListener('click', triggerInput)
+    input.addEventListener('change', changeHandler)
+}
